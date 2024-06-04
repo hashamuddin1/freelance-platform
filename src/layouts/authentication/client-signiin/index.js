af7a189863987ca83e4685bedd95731177dcf858
hashamuddin1 <hashamlaptop@gmail.com>
2024-06-04 21:44:12 +0500

    API Integration start

--- src/layouts/authentication/client-signiin/index.js
+++ src/layouts/authentication/client-signiin/index.js
@@ -1,18 +1,3 @@
-/**
-=========================================================
-* Material Dashboard 2 React - v2.2.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/material-dashboard-react
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-
-Coded by www.creative-tim.com
-
- =========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-*/
-
 import { useState } from "react";
 
 // react-router-dom components
@@ -33,66 +18,156 @@ import BasicLayout from "layouts/authentication/components/BasicLayout";
 // Images
 import bgImage from "assets/images/bg-sign-in-basic.jpeg";
 
-function Basic() {
-  return (
-    <BasicLayout image={bgImage}>
-      <Card>
-        <MDBox
-          variant="gradient"
-          bgColor="info"
-          borderRadius="lg"
-          coloredShadow="info"
-          mx={2}
-          mt={-3}
-          p={2}
-          mb={1}
-          textAlign="center"
-        >
-          <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
-            Sign in
-          </MDTypography>
-        </MDBox>
-        <MDBox pt={4} pb={3} px={3}>
-          <MDBox component="form" role="form">
-            <MDBox mb={2}>
-              <MDInput type="email" label="Email" fullWidth />
-            </MDBox>
-            <MDBox mb={2}>
-              <MDInput type="password" label="Password" fullWidth />
-            </MDBox>
+import { APP_URL } from "../../../config";
+import axios from "axios";
+import Alert from "@mui/material/Alert";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useNavigate } from "react-router-dom";
 
-            <MDBox mt={4} mb={1}>
-              <MDTypography
-                component={Link}
-                to="/completeprofile/Client"
-                variant="button"
-                color="white"
-                fontWeight="medium"
-              >
-                <MDButton variant="gradient" color="info" fullWidth>
-                  sign in
+function Basic() {
+  const navigate = useNavigate();
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const changeEmailAddress = (event) => {
+    setEmailAddress(event.target.value);
+  };
+
+  const changePassword = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const submitResult = async (event) => {
+    setIsLoading(true);
+    event.preventDefault();
+
+    try {
+      if (emailAddress === "" || password === "") {
+        setIsLoading(false);
+        return simulateError("First Fill All The Fields");
+      }
+      console.log(APP_URL);
+      const response = await axios.post(`${APP_URL}/api/userSignIn`, {
+        emailAddress,
+        password,
+        role: "client",
+      });
+      if (response.data.success === true) {
+        const token = response.data.token;
+
+        const checkToken = localStorage.getItem("token");
+        if (checkToken) {
+          localStorage.removeItem("token");
+        }
+
+        localStorage.setItem("token", token);
+        setIsLoading(false);
+        return showSuccessModal("Client Sign In Successfully");
+      }
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message ? error.response.data.message : error.message;
+      if (message === "First Complete Your Profile") {
+        navigate("/completeprofile/Client");
+      } else {
+        setIsLoading(false);
+        return simulateError(message);
+      }
+    }
+  };
+
+  const showSuccessModal = (successMessage) => {
+    setSuccess(successMessage);
+    setTimeout(() => {
+      setSuccess(null);
+      navigate("/dashboard");
+    }, 3000);
+  };
+
+  const simulateError = (errorMessage) => {
+    setError(errorMessage);
+    setTimeout(() => {
+      setError(null);
+    }, 3000);
+  };
+  return (
+    <>
+      {error && (
+        <Alert variant="filled" severity="error">
+          {error}
+        </Alert>
+      )}
+
+      {success && (
+        <Alert variant="filled" severity="success">
+          {success}
+        </Alert>
+      )}
+      <BasicLayout image={bgImage}>
+        <Card>
+          <MDBox
+            variant="gradient"
+            bgColor="info"
+            borderRadius="lg"
+            coloredShadow="info"
+            mx={2}
+            mt={-3}
+            p={2}
+            mb={1}
+            textAlign="center"
+          >
+            <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
+              Sign in
+            </MDTypography>
+          </MDBox>
+          <MDBox pt={4} pb={3} px={3}>
+            <MDBox component="form" role="form">
+              <MDBox mb={2}>
+                <MDInput
+                  value={emailAddress}
+                  onChange={changeEmailAddress}
+                  type="email"
+                  label="Email"
+                  fullWidth
+                />
+              </MDBox>
+              <MDBox mb={2}>
+                <MDInput
+                  value={password}
+                  onChange={changePassword}
+                  type="password"
+                  label="Password"
+                  fullWidth
+                />
+              </MDBox>
+
+              <MDBox mt={4} mb={1}>
+                <MDButton onClick={submitResult} variant="gradient" color="info" fullWidth>
+                  {isLoading ? <CircularProgress size={24} color="inherit" /> : "sign in"}
                 </MDButton>
-              </MDTypography>
-            </MDBox>
-            <MDBox mt={3} mb={1} textAlign="center">
-              <MDTypography variant="button" color="text">
-                Don&apos;t have an account?{" "}
-                <MDTypography
-                  component={Link}
-                  to="/authentication/client-signup"
-                  variant="button"
-                  color="info"
-                  fontWeight="medium"
-                  textGradient
-                >
-                  Sign up
+              </MDBox>
+              <MDBox mt={3} mb={1} textAlign="center">
+                <MDTypography variant="button" color="text">
+                  Don&apos;t have an account?{" "}
+                  <MDTypography
+                    component={Link}
+                    to="/authentication/client-signup"
+                    variant="button"
+                    color="info"
+                    fontWeight="medium"
+                    textGradient
+                  >
+                    Sign up
+                  </MDTypography>
                 </MDTypography>
-              </MDTypography>
+              </MDBox>
             </MDBox>
           </MDBox>
-        </MDBox>
-      </Card>
-    </BasicLayout>
+        </Card>
+      </BasicLayout>
+    </>
   );
 }
 
--- src/layouts/completeprofile/Client/index.js
+++ src/layouts/completeprofile/Client/index.js
@@ -19,53 +19,150 @@ import CoverLayout from "layouts/authentication/components/CoverLayout";
 // Images
 import bgImage from "assets/images/bg-sign-up-cover.jpeg";
 
+import { APP_URL } from "../../../config";
+import axios from "axios";
+import React, { useState } from "react";
+import Alert from "@mui/material/Alert";
+import CircularProgress from "@mui/material/CircularProgress";
+import { useNavigate } from "react-router-dom";
+
 function Client() {
+  const navigate = useNavigate();
+  const [description, setDescription] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [error, setError] = useState(null);
+  const [success, setSuccess] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleDescription = (event) => {
+    setDescription(event.target.value);
+  };
+
+  const handlePhoneNumber = (event) => {
+    setPhoneNumber(event.target.value);
+  };
+
+  const submitResult = async (event) => {
+    setIsLoading(true);
+    event.preventDefault();
+
+    try {
+      if (description === "" || phoneNumber === "") {
+        setIsLoading(false);
+        return simulateError("First Fill All The Fields");
+      }
+      const token = localStorage.getItem("token");
+      if (!token) {
+        navigate("/authentication/client-signup");
+      }
+
+      const response = await axios.post(
+        `${APP_URL}/api/completeProfile`,
+        {
+          phoneNumber: phoneNumber,
+          description: description,
+        },
+        { headers: { "x-access-token": token } }
+      );
+      if (response.data.success === true) {
+        const token = response.data.token;
+
+        const checkToken = localStorage.getItem("token");
+        if (checkToken) {
+          localStorage.removeItem("token");
+        }
+
+        localStorage.setItem("token", token);
+        setIsLoading(false);
+        return showSuccessModal("Client Complete Profile Successfully");
+      }
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message ? error.response.data.message : error.message;
+      setIsLoading(false);
+      return simulateError(message);
+    }
+  };
+
+  const showSuccessModal = (successMessage) => {
+    setSuccess(successMessage);
+    setTimeout(() => {
+      setSuccess(null);
+      navigate("/dashboard");
+    }, 3000);
+  };
+
+  const simulateError = (errorMessage) => {
+    setError(errorMessage);
+    setTimeout(() => {
+      setError(null);
+    }, 3000);
+  };
   return (
-    <CoverLayout image={bgImage}>
-      <Card>
-        <MDBox
-          variant="gradient"
-          bgColor="info"
-          borderRadius="lg"
-          coloredShadow="success"
-          mx={2}
-          mt={-3}
-          p={3}
-          mb={1}
-          textAlign="center"
-        >
-          <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
-            Complete Your Profile
-          </MDTypography>
-          <MDTypography display="block" variant="button" color="white" my={1}>
-            Enter your details
-          </MDTypography>
-        </MDBox>
-        <MDBox pt={4} pb={3} px={3}>
-          <MDBox component="form" role="form">
-            <MDBox mb={2}>
-              <MDInput type="text" label="Description" variant="standard" fullWidth />
-            </MDBox>
-            <MDBox mb={2}>
-              <MDInput type="text" label="Phone Number" variant="standard" fullWidth />
-            </MDBox>
-            <MDTypography
-              component={Link}
-              to="/dashboard"
-              variant="button"
-              color="white"
-              fontWeight="medium"
-            >
-              <MDBox mt={4} mb={1}>
-                <MDButton variant="gradient" color="info" fullWidth>
-                  Create Profile
-                </MDButton>
-              </MDBox>
+    <>
+      {error && (
+        <Alert variant="filled" severity="error">
+          {error}
+        </Alert>
+      )}
+
+      {success && (
+        <Alert variant="filled" severity="success">
+          {success}
+        </Alert>
+      )}
+      <CoverLayout image={bgImage}>
+        <Card>
+          <MDBox
+            variant="gradient"
+            bgColor="info"
+            borderRadius="lg"
+            coloredShadow="success"
+            mx={2}
+            mt={-3}
+            p={3}
+            mb={1}
+            textAlign="center"
+          >
+            <MDTypography variant="h4" fontWeight="medium" color="white" mt={1}>
+              Complete Your Profile
+            </MDTypography>
+            <MDTypography display="block" variant="button" color="white" my={1}>
+              Enter your details
             </MDTypography>
           </MDBox>
-        </MDBox>
-      </Card>
-    </CoverLayout>
+          <MDBox pt={4} pb={3} px={3}>
+            <MDBox component="form" role="form">
+              <MDBox mb={2}>
+                <MDInput
+                  type="text"
+                  value={description}
+                  onChange={handleDescription}
+                  label="Description"
+                  variant="standard"
+                  fullWidth
+                />
+              </MDBox>
+              <MDBox mb={2}>
+                <MDInput
+                  type="text"
+                  value={phoneNumber}
+                  onChange={handlePhoneNumber}
+                  label="Phone Number"
+                  variant="standard"
+                  fullWidth
+                />
+              </MDBox>
+              <MDBox mt={4} mb={1}>
+                <MDButton onClick={submitResult} variant="gradient" color="info" fullWidth>
+                  {isLoading ? <CircularProgress size={24} color="inherit" /> : "Create Profile"}
+                </MDButton>
+              </MDBox>
+            </MDBox>
+          </MDBox>
+        </Card>
+      </CoverLayout>
+    </>
   );
 }
 
