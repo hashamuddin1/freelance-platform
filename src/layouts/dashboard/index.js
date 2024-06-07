a7e64b43935bc19cfdbab1189ca2c08ce15baa40
hashamuddin1 <hashamlaptop@gmail.com>
2024-06-07 05:51:15 +0500

    Assign Order API Integrated

--- src/layouts/dashboard/index.js
+++ src/layouts/dashboard/index.js
@@ -68,10 +68,51 @@ function Dashboard() {
   const [error, setError] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
   const [agentData, setAgentData] = useState(null);
+  const [agentId, setAgentId] = useState(null);
+  const [agentPrice, setAgentPrice] = useState(null);
+  const [orderTitle, setOrderTitle] = useState(null);
+  const [orderDescription, setOrderDescription] = useState(null);
+  const [success, setSuccess] = useState(null);
 
   const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
+  const handleOpen = (agentId, price) => {
+    setOpen(true);
+    setAgentId(agentId);
+    setAgentPrice(price);
+  };
   const handleClose = () => setOpen(false);
+
+  const submitResult = async (event) => {
+    event.preventDefault();
+
+    try {
+      if (orderTitle === null || orderDescription === null) {
+        return simulateError("First Fill All The Fields");
+      }
+      const token = localStorage.getItem("token");
+      if (!token) {
+        navigate("/authentication/first-page");
+      }
+      const response = await axios.post(
+        `${APP_URL}/api/assignOrder`,
+        {
+          title: orderTitle,
+          description: orderDescription,
+          agentId: agentId,
+          price: agentPrice,
+        },
+        { headers: { "x-access-token": token } }
+      );
+      if (response.data.success === true) {
+        const checkToken = localStorage.getItem("token");
+        return showSuccessModal("Order Created Successfully");
+      }
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message ? error.response.data.message : error.message;
+      return simulateError(message);
+    }
+  };
 
   const columns = [
     { field: "id", headerName: "ID", width: 90 },
@@ -100,7 +141,10 @@ function Dashboard() {
       width: 100,
       renderCell: (params) => (
         <Button
-          onClick={handleOpen}
+          onClick={(e) => {
+            e.stopPropagation();
+            handleOpen(params.row.id, params.row.price);
+          }}
           className="hireBtn"
           variant="contained"
           sx={{ backgroundColor: "green", color: "white" }}
@@ -187,6 +231,21 @@ function Dashboard() {
     }, 3000);
   };
 
+  const showSuccessModal = (successMessage) => {
+    setSuccess(successMessage);
+    setTimeout(() => {
+      setSuccess(null);
+    }, 3000);
+  };
+
+  const changeOrderTitle = (event) => {
+    setOrderTitle(event.target.value);
+  };
+
+  const changeOrderDescription = (event) => {
+    setOrderDescription(event.target.value);
+  };
+
   if (isLoading) {
     return (
       <>
@@ -200,6 +259,7 @@ function Dashboard() {
   }
   let rows = [];
 
+  console.log({ agentData, data });
   if (agentData) {
     rows = agentData.map(({ _id, fullName, emailAddress, price }) => ({
       fullName,
@@ -216,216 +276,227 @@ function Dashboard() {
     setAnchorElUser(null);
   };
 
-  return (
-    <>
-      {error && (
-        <Alert variant="filled" severity="error">
-          {error}
-        </Alert>
-      )}
-      <DashboardLayout>
-        <AppBar position="static">
-          <Container maxWidth="xl">
-            <Toolbar disableGutters>
-              <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
-                <IconButton
-                  size="large"
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                  onClick={handleOpenNavMenu}
-                  color="inherit"
-                >
-                  <MenuIcon />
-                </IconButton>
-                <Menu
-                  id="menu-appbar"
-                  anchorEl={anchorElNav}
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "left",
-                  }}
-                  open={Boolean(anchorElNav)}
-                  onClose={handleCloseNavMenu}
-                  sx={{
-                    display: { xs: "block", md: "none" },
-                  }}
-                >
-                  {pages.map((page) => (
-                    <MenuItem key={page.page} onClick={handleCloseNavMenu}>
-                      <MDTypography
-                        component={Link}
-                        to={`/${page.link}`}
-                        variant="button"
-                        color="white"
-                        fontWeight="medium"
-                      >
-                        {page.page}
-                      </MDTypography>
-                    </MenuItem>
-                  ))}
-                </Menu>
-              </Box>
-              <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-                {pages.map((page) => (
-                  <MDTypography
-                    component={Link}
-                    to={`/${page.link}`}
-                    variant="button"
-                    color="black"
-                    key={page.page}
-                    fontWeight="medium"
-                    sx={{ marginRight: 5 }}
+  if (data) {
+    return (
+      <>
+        {error && (
+          <Alert variant="filled" severity="error">
+            {error}
+          </Alert>
+        )}
+        {success && (
+          <Alert variant="filled" severity="success">
+            {success}
+          </Alert>
+        )}
+        <DashboardLayout>
+          <AppBar position="static">
+            <Container maxWidth="xl">
+              <Toolbar disableGutters>
+                <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
                   >
-                    {page.page}
-                  </MDTypography>
-                ))}
-              </Box>
-
-              <Box sx={{ flexGrow: 0 }}>
-                <Menu
-                  sx={{ mt: "45px" }}
-                  id="menu-appbar"
-                  anchorEl={anchorElUser}
-                  anchorOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  keepMounted
-                  transformOrigin={{
-                    vertical: "top",
-                    horizontal: "right",
-                  }}
-                  open={Boolean(anchorElUser)}
-                  onClose={handleCloseUserMenu}
-                >
-                  {settings.map((setting) => (
-                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                      <Typography textAlign="center">{setting}</Typography>
-                    </MenuItem>
+                    <MenuIcon />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                      vertical: "bottom",
+                      horizontal: "left",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "left",
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={handleCloseNavMenu}
+                    sx={{
+                      display: { xs: "block", md: "none" },
+                    }}
+                  >
+                    {pages.map((page) => (
+                      <MenuItem key={page.page} onClick={handleCloseNavMenu}>
+                        <MDTypography
+                          component={Link}
+                          to={`/${page.link}`}
+                          variant="button"
+                          color="white"
+                          fontWeight="medium"
+                        >
+                          {page.page}
+                        </MDTypography>
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                </Box>
+                <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+                  {pages.map((page) => (
+                    <MDTypography
+                      component={Link}
+                      to={`/${page.link}`}
+                      variant="button"
+                      color="black"
+                      key={page.page}
+                      fontWeight="medium"
+                      sx={{ marginRight: 5 }}
+                    >
+                      {page.page}
+                    </MDTypography>
                   ))}
-                </Menu>
-              </Box>
-            </Toolbar>
-          </Container>
-        </AppBar>
-        <MDBox py={3}>
-          <Grid container spacing={3}>
-            <Grid item xs={12} md={6} lg={3}>
-              <MDBox mb={1.5}>
-                <ComplexStatisticsCard
-                  color="dark"
-                  icon="weekend"
-                  title="Total Orders"
-                  count={281}
-                />
-              </MDBox>
+                </Box>
+
+                <Box sx={{ flexGrow: 0 }}>
+                  <Menu
+                    sx={{ mt: "45px" }}
+                    id="menu-appbar"
+                    anchorEl={anchorElUser}
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    open={Boolean(anchorElUser)}
+                    onClose={handleCloseUserMenu}
+                  >
+                    {settings.map((setting) => (
+                      <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                        <Typography textAlign="center">{setting}</Typography>
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                </Box>
+              </Toolbar>
+            </Container>
+          </AppBar>
+          <MDBox py={3}>
+            <Grid container spacing={3}>
+              <Grid item xs={12} md={6} lg={3}>
+                <MDBox mb={1.5}>
+                  <ComplexStatisticsCard
+                    color="dark"
+                    icon="weekend"
+                    title="Total Orders"
+                    count={281}
+                  />
+                </MDBox>
+              </Grid>
+              <Grid item xs={12} md={6} lg={3}>
+                <MDBox mb={1.5}>
+                  <ComplexStatisticsCard icon="leaderboard" title="Pending Orders" count="2,300" />
+                </MDBox>
+              </Grid>
+              <Grid item xs={12} md={6} lg={3}>
+                <MDBox mb={1.5}>
+                  <ComplexStatisticsCard
+                    color="success"
+                    icon="store"
+                    title="Completed Order"
+                    count="34k"
+                  />
+                </MDBox>
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={6} lg={3}>
-              <MDBox mb={1.5}>
-                <ComplexStatisticsCard icon="leaderboard" title="Pending Orders" count="2,300" />
-              </MDBox>
+          </MDBox>
+          <MDBox mt={4.5}></MDBox>
+          <MDBox>
+            <Grid item xs={12} md={6} lg={4}>
+              <TextField
+                style={{ marginRight: 10 }}
+                id="outlined-read-only-input"
+                label="Full Name"
+                defaultValue={data.fullName}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <TextField
+                style={{ marginRight: 10 }}
+                id="outlined-read-only-input"
+                label="Email Address"
+                defaultValue={data.emailAddress}
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <TextField
+                style={{ marginRight: 10 }}
+                id="outlined-read-only-input"
+                label="Account"
+                defaultValue="Client"
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
             </Grid>
-            <Grid item xs={12} md={6} lg={3}>
-              <MDBox mb={1.5}>
-                <ComplexStatisticsCard
-                  color="success"
-                  icon="store"
-                  title="Completed Order"
-                  count="34k"
-                />
-              </MDBox>
-            </Grid>
-          </Grid>
-        </MDBox>
-        <MDBox mt={4.5}></MDBox>
-        <MDBox>
-          <Grid item xs={12} md={6} lg={4}>
-            <TextField
-              style={{ marginRight: 10 }}
-              id="outlined-read-only-input"
-              label="Full Name"
-              defaultValue={data.fullName}
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-            <TextField
-              style={{ marginRight: 10 }}
-              id="outlined-read-only-input"
-              label="Email Address"
-              defaultValue={data.emailAddress}
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-            <TextField
-              style={{ marginRight: 10 }}
-              id="outlined-read-only-input"
-              label="Account"
-              defaultValue="Client"
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-          </Grid>
-        </MDBox>
-        <MDBox mt={4.5}></MDBox>
-        <Box sx={{ height: 400, width: "100%" }}>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            initialState={{
-              pagination: {
-                paginationModel: {
-                  pageSize: 5,
+          </MDBox>
+          <MDBox mt={4.5}></MDBox>
+          <Box sx={{ height: 400, width: "100%" }}>
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              initialState={{
+                pagination: {
+                  paginationModel: {
+                    pageSize: 5,
+                  },
                 },
-              },
-            }}
-            pageSizeOptions={[5]}
-            checkboxSelection
-            disableRowSelectionOnClick
-          />
-        </Box>
-      </DashboardLayout>
-      <Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description"
-      >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Insert Order Details
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            <TextField
-              id="outlined-basic"
-              label="Title"
-              variant="outlined"
-              sx={{ marginBottom: 2 }}
+              }}
+              pageSizeOptions={[5]}
+              checkboxSelection
+              disableRowSelectionOnClick
             />
-            <div>
+          </Box>
+        </DashboardLayout>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Insert Order Details
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
               <TextField
                 id="outlined-basic"
-                label="Description"
-                sx={{ marginBottom: 2 }}
+                label="Title"
                 variant="outlined"
+                sx={{ marginBottom: 2 }}
+                value={orderTitle}
+                onChange={changeOrderTitle}
               />
-            </div>
-            <Button variant="contained" className="btn">
-              Submit
-            </Button>
-          </Typography>
-        </Box>
-      </Modal>
-    </>
-  );
+              <div>
+                <TextField
+                  id="outlined-basic"
+                  label="Description"
+                  sx={{ marginBottom: 2 }}
+                  variant="outlined"
+                  value={orderDescription}
+                  onChange={changeOrderDescription}
+                />
+              </div>
+              <Button variant="contained" onClick={submitResult} className="btn">
+                Submit
+              </Button>
+            </Typography>
+          </Box>
+        </Modal>
+      </>
+    );
+  }
 }
 
 export default Dashboard;
